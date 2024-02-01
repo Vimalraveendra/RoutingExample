587188b1cc367f2253833a1c57846790ee1d8216
Vimal Raveendran <vimalraveendra@gmail.com>
2024-02-01 14:54:41 +0100

    added page-not-found component, added rdirect to page not found if the url is not found

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -11,10 +11,19 @@ export class EditServerComponent  implements OnInit{
   server:{id:number,name:string,status:string};
   serverName="";
   serverStatus="";
+  allowEdit=false;
+
   constructor(
     private serversService:ServersService,
+    private route:ActivatedRoute
     ){}
   ngOnInit() {
+      this.route.queryParams.subscribe(
+        (queryParams:Params)=>{
+          console.log(typeof(queryParams['allowEdit']))
+           this.allowEdit=queryParams['allowEdit']==='1' ? true:false;
+        }
+      )
       this.server=this.serversService.getServer(1);
       this.serverName=this.server.name;
       this.serverStatus=this.server.status; 
